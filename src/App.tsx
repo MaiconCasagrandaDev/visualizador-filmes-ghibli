c991eb0df02e68674c515f9a337542af8e6d022b
Maicon Casagranda <maiconcasag@gmail.com>
2026-09-09 17:55:59 -0300

    feat: adiciona busca de filmes com o useState/useEffect e configura Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import type { Film } from './types/Film'
 
 function App() {
 
+  const [films, setFilms] = useState<Film[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    async function fetchFilms() {
+      const result = await fetch("https://ghibliapi.vercel.app/films");
+      const data: Film[] = await result.json(); 
+      const dataOrdered = data.sort((filmA, filmB) => 
+        filmA.title.localeCompare(filmB.title)
+      );
+      setFilms(dataOrdered);
+
+    }
+
+    fetchFilms();
+
+  }, []);
+
   return (
     <>
+      <div>
+        {loading && <p className="text-red-500">Loading...</p>}
+        {error && <p> Erro: {error}</p>}
 
+        <ul>
+          {films.map((film) => (
+            <li  key={film.id}> {film.title}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }

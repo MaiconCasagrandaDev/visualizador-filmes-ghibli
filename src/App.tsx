e0c790bba531831708d447a51a8afdb8d6056eb6
Maicon Casagranda <maiconcasag@gmail.com>
2026-09-15 17:06:32 -0300

    chore: cria estrutura de páginas (layout e FilmDetails)

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,9 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import type { Film } from './types/Film'
+import Layout from "./pages/Layout"
+
 
 function App() {
-
-  const [films, setFilms] = useState<Film[]>([])
-  const [loading, setLoading] = useState<boolean>(true)
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    async function fetchFilms() {
-      try {
-        const result = await fetch("https://ghibliapi.vercel.app/films");
-        if (!result.ok) {
-          throw new Error("Erro ao buscar os filmes");
-        }
-        const data: Film[] = await result.json();
-        const dataOrdered = data.sort((filmA, filmB) =>
-          filmA.title.localeCompare(filmB.title)
-        );
-        const dataSliced = dataOrdered.slice(0, 10);
-
-        setFilms(dataSliced);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError("Erro ao buscar os filmes");
-        }
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchFilms();
-
-  }, []);
-
   return (
-    <>
-      <div>
-        {loading && <p className="text-red-500">Loading...</p>}
-        {error && <p> Erro: {error}</p>}
-
-        <ul>
-          {films.map((film) => (
-            <li key={film.id}>
-              {film.title}
-              <img src={film.movie_banner} alt={film.title} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <Layout />
   )
 }
 

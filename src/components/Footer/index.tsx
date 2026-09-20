d2656fc82e3428cb2dd02b03a55945bf7351b0e8
Maicon Casagranda <maiconcasag@gmail.com>
2026-09-20 00:16:50 -0300

    style: adiciona estilo ao footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,9 @@
 export const Footer = () => {
     return (
-        <footer>
-            <p>&copy; 2026 Studio Ghibli Films. All rights reserved.</p>
+        <footer className="bg-footer-bg text-footer-hover py-6 mt-10">
+            <div className="max-w-7xl mx-auto px-6 text-center font-body text-sm">
+                <p>&copy; 2026 Studio Ghibli Films. All rights reserved.</p>
+            </div>
         </footer>
     )
 }
